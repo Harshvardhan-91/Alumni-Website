9801492dc419f8b9e3f0008f2e13273cba10ad25
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-01-31 18:17:32 +0530

    added backend url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.PROD 
-  ? 'https://alumni-website-backend2.onrender.com'
+  ? 'https://alumni-website-backend2.onrender.com' // Your deployed backend URL
   : 'http://localhost:5000';
 
 export default API_BASE_URL; 
